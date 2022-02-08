a9a51dd530ff7db56141321daacd739e2f239857
weston-bailey <wdbailey87@gmail.com>
2022-02-07 19:10:59 -0800

    update webpack template

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,18 @@
 import css from '../css/styles.css'
 import { Engine } from './lib/modules.js'
 
-function init() {
-  // find the smaller side and use that
-  const side = window.innerWidth < window.innerHeight ? window.innerWidth - 10 : window.innerHeight - 10 
-  const gameArgs = {
-    // html canvas element id
-    divId: 'canvas-container',
-    // cnvas height in pixels
-    height: side,       
-    width: side,
-    // level to start at
-    currentLevel: 0         
-  }
-
-  new Engine(gameArgs) 
-}
-
-document.addEventListener('DOMContentLoaded', init)
+document.addEventListener('DOMContentLoaded', () => {
+    // find the smaller side and use that
+    const side = window.innerWidth < window.innerHeight ? window.innerWidth - 10 : window.innerHeight - 10 
+    const gameArgs = {
+      // html canvas element id
+      divId: 'canvas-container',
+      // cnvas height in pixels
+      height: side,       
+      width: side,
+      // level to start at
+      currentLevel: 0         
+    }
+  
+    new Engine(gameArgs) 
+})
